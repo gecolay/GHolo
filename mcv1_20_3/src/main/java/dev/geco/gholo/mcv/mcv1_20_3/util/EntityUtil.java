9135ccb80248cca64416ba4d2dff106855bb1eb6
Gecolay <Discord.Benjamin@web.de>
2024-12-10 14:10:46 +0100

    Fix maven project structure

---
+++ mcv1_20_3/src/main/java/dev/geco/gholo/mcv/mcv1_20_3/util/EntityUtil.java
@@ -0,0 +1,21 @@
+package dev.geco.gholo.mcv.mcv1_20_3.util;
+
+import dev.geco.gholo.GHoloMain;
+import dev.geco.gholo.mcv.mcv1_20_3.objects.*;
+import dev.geco.gholo.objects.*;
+import dev.geco.gholo.util.*;
+
+public class EntityUtil implements IEntityUtil {
+
+    private final GHoloMain GPM;
+
+    public EntityUtil(GHoloMain GPluginMain) { GPM = GPluginMain; }
+
+    @Override
+    public void loadHoloRowEntity(GHoloRow HoloRow) {
+        GHoloRowEntity holoRowEntity = new GHoloRowEntity(HoloRow);
+        HoloRow.setHoloRowEntity(holoRowEntity);
+        holoRowEntity.loadHoloRow();
+    }
+
+}
