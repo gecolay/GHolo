f4c9614f6b0f7edc59cd73b0b863bb7e9cdb2d57
Gecolay <Discord.Benjamin@web.de>
2024-12-19 22:33:26 +0100

    Update gradle build

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,31 +3,21 @@ plugins {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    toolchain.languageVersion = JavaLanguageVersion.of(17)
 }
 
-tasks {
-    compileJava {
-        options.release.set(17)
-    }
-
-    publishToMavenLocal {
-        dependsOn(build)
-    }
-
-    publishing {
-        publications {
-            create<MavenPublication>("maven") {
-                groupId = project.group.toString()
-                artifactId = project.name
-                version = project.version.toString()
-                from(project.components["java"])
-            }
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+            from(project.components["java"])
         }
     }
 }
