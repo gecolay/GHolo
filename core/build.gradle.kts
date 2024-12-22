b0f0097b42406000cfc2d4df95f3d1feda87dc02
Gecolay <Discord.Benjamin@web.de>
2024-12-22 19:22:30 +0100

    Update publish

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,12 +3,16 @@ plugins {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
     compileOnly("me.clip:placeholderapi:2.11.6")
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(17)
+    toolchain.languageVersion = JavaLanguageVersion.of(21)
+}
+
+tasks.compileJava {
+    options.release = 17
 }
 
 publishing {
