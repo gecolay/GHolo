a36a7526f7f4ad8d7b66e9ae34cafa3138036d6e
kamilleon <25117301+kamilleon@users.noreply.github.com>
2025-02-19 22:41:44 +0100

    better relocate command, allows relative relocation with ~ characters

--- core/src/main/java/dev/geco/gholo/cmd/GHoloCommand.java
+++ core/src/main/java/dev/geco/gholo/cmd/GHoloCommand.java
@@ -200,7 +200,21 @@ public class GHoloCommand implements CommandExecutor {
                 }
                 try {
                     Location location = holo.getLocation();
-                    location.set(Double.parseDouble(args[2]), Double.parseDouble(args[3]), Double.parseDouble(args[4]));
+
+                    double xLocation = args[2].startsWith("~") ?
+                        location.getX() + (args[2].length() > 1 ? Double.parseDouble(args[2].substring(1)) : 0) :
+                        Double.parseDouble(args[2]);
+
+                    double yLocation = args[3].startsWith("~") ?
+                        location.getY() + (args[3].length() > 1 ? Double.parseDouble(args[3].substring(1)) : 0) :
+                        Double.parseDouble(args[3]);
+
+                    double zLocation = args[4].startsWith("~") ?
+                        location.getZ() + (args[4].length() > 1 ? Double.parseDouble(args[4].substring(1)) : 0) :
+                        Double.parseDouble(args[4]);
+
+                    location.set(xLocation, yLocation, zLocation);
+
                     gHoloMain.getHoloService().updateHoloLocation(holo, location);
                     gHoloMain.getMessageService().sendMessage(sender, "Messages.command-gholo-relocate", "%Holo%", holo.getId());
                 } catch(NumberFormatException e) {
